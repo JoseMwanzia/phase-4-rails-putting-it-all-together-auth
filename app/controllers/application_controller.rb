9721ee08c4515c7c8005e07bbd60d3cf2f25ccb1
joseph mwanzia <makamwanzia@gmail.com>
2023-04-09 03:52:22 +0300

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+before_action :authorize
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+def authorize
+  @current_user = User.find_by(id: session[:user_id])
+  render json: {errors: ["Not authorized"] },status: :unauthorized unless @current_user
+end
+
+
+  def render_invalid_response(execption)
+    render json: { errors: execption.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
