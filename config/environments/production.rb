c6bb85da5fddb1478d539889f63b64a8947b923c
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-17 01:56:56 +0300

    build: update files for build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE']
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
